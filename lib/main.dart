12bac0c312b49eecaf422338737a8a69f496ac47
yangzhi <1027627051@qq.com>
2023-04-26 18:13:02 +0800

    开发 钱包首页

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mvcwallet/utils/SimColor.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +8,125 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+            title: Container(
+                child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text("data"),
+            Text("good"),
+            Text("good"),
+            Text("good"),
+            Text("good")
+          ],
+        ))),
+        body: HomePage(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            height: 240,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+                // color: Colors.red,
+                image: DecorationImage(
+              image: AssetImage("images/bg_img_space.png"),
+            )
+                // image: AssetImage("images/icon.png"),)
+                ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Balance",
+                  style: TextStyle(
+                      color: Color(SimColor.deaful_txt_half_color),
+                      fontSize: 16),
+                ),
+                const SizedBox(
+                  height: 20,
+                ), //SimColor.deaful_txt_color
+                const Text(
+                  "\$34,201.25",
+                  style: TextStyle(
+                      color: Color(SimColor.deaful_txt_color), fontSize: 40),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "images/icon.png",
+                      width: 20,
+                      height: 20,
+                    ),
+                    const SizedBox(width: 10),
+                    const Text(
+                      "21347.32 Spacessss",
+                      style: TextStyle(fontSize: 14),
+                    )
+                  ],
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          //这里是两个 Button
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+            child: Row(
+              //这里写2个 Button
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                    flex: 1,
+                    child:SizedBox(
+                      height: 44,
+                      child: ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                  const Color(SimColor.deaful_txt_color))),
+                          child: const Text("Request",style: TextStyle(fontSize: 16))),
+                    )
+                ),
+                const SizedBox(width: 20),
+                Expanded(
+                    flex: 1,
+                    child: SizedBox(
+                        height: 44,
+                        child:ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                  const Color(SimColor.color_button_blue))),
+                          child: const Text("Send",style: TextStyle(fontSize: 16)),
+                        )
+                    )
+                )
+              ],
             ),
-          ],
-        ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/utils/SimColor.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class SimColor {
+  static const int deaful_txt_color=0xff303133;
+  // static const String deaful_txt_color_str="#303133".toColor();
+  static const int deaful_txt_half_color=0x80303133;
+  static const int color_button_blue=0xff171AFF;
+
+}
+
