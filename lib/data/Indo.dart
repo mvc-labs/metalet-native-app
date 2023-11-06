96e0bbaa6149bfddbd108da1a7ed14b8b85c12d5
yangzhi <1027627051@qq.com>
2023-11-06 18:30:01 +0800

    bybtc commit2 提交部分

--- lib/data/Indo.dart
+++ lib/data/Indo.dart
@@ -1,31 +1,26 @@
-
-
 import 'package:mvcwallet/utils/Constants.dart';
 
 //wallet
-abstract class  Indo{
+abstract class Indo {
+  void addWallet(String walletName, String mnemoni, String path);
 
-  void addWallet(String walletName,String mnemoni,String path);
-
-  void createWallet(String walletName,String path);
+  void createWallet(String walletName, String path);
 
   void switchWallet(Wallet? wallet);
-
-
 }
-
-
 
 //send NFT
-abstract class SendNftIndo{
+abstract class SendNftIndo {
   void sendCancel();
+
   void sendConfirm(String sendAddress);
-
 }
 
-abstract class SendFtIndo{
+abstract class SendFtIndo {
   void sendCancel();
-  void sendConfirm(String sendAddress,String sendAmount);
-  // btc change1111111
-}
 
+  void sendConfirm(String sendAddress, String sendAmount);
+// btc change1111111  333
+// btc change1111111
+// 我是没问题的代码 最终将提交
+}
